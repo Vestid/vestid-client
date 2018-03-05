91b53e19c394a4773189dbd07c47825a11c632d8
Dallin Parker <dallin.r.parker@gmail.com>
2018-03-04 19:24:56 -0500

    colors are reflecting properly based on validation

--- src/containers/seekingLoan/reducer.js
+++ src/containers/seekingLoan/reducer.js
@@ -1,13 +1,14 @@
 import {fromJS} from 'immutable'
 import actionTypes from './actions/actionTypes'
+import {vgray} from '../../styles/colors_'
 
 const initialState = fromJS({
-	firstName: {placeholder: 'john', verified: false, color: null},
-	lastName: {placeholder: 'doe', verified: false, color: null},
-	email: {placeholder: 'john_doe@example.com', verified: false, color: null},
-	seekingLoanAmount: {placeholder: 'loan amount you are seeking', verified: false, color: null},
-	businessType: {placeholder: 'type of business, e.g. technology, culinary, etc.', verified: false, color: null},
-	location: {placeholder: 'where are you located', verified: false, color: null},
+	firstName: {placeholder: 'john', verified: false, color: `${vgray}`},
+	lastName: {placeholder: 'doe', verified: false, color: `${vgray}`},
+	email: {placeholder: 'john_doe@example.com', verified: false, color: `${vgray}`},
+	seekingLoanAmount: {placeholder: 'loan amount you are seeking', verified: false, color: `${vgray}`},
+	businessType: {placeholder: 'type of business, e.g. technology, culinary, etc.', verified: false, color: `${vgray}`},
+	location: {placeholder: 'where are you located', verified: false, color: `${vgray}`},
 	description: {placeholder: 'please describe in a couple sentences the "why" behind your business and what this loan will be used for', verified: false, color: null
 	}
 })

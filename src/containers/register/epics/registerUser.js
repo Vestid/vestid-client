689bdc8e2bf63c789f3b369783d0d7d6037e4c24
Dallin Parker <dallin.r.parker@gmail.com>
2018-03-10 18:52:04 -0500

    WIP: registration is working

--- src/containers/register/epics/registerUser.js
+++ src/containers/register/epics/registerUser.js
@@ -1,6 +1,7 @@
 import actionTypes from '../actions/actionTypes'
 import {Observable} from 'rxjs/Observable'
 import AuthService from '../../../services/fireAuth'
+import {updateAuthInfo} from '../../../services/fireAuth/actions/actions'
 
 export default (action$, store) => {
     return action$.ofType(actionTypes.SUBMIT_REGISTER_FORM)
@@ -9,7 +10,13 @@ export default (action$, store) => {
 	      console.log('signup properties: ', firstName, lastName, email, password)
 	      return Observable.fromPromise(AuthService.registerUser(email, password))
 		      .mergeMap(response => {
-		      	console.log('auth response: ', response)
+			      console.log('auth response: ', response)
+		      	if(response.uid){
+							const {emailVerified, uid} = response
+				      const userInfo = Object.assign({}, {['authed']: true, firstName, lastName, emailVerified, uid})
+				      console.log('userInfo: ', userInfo)
+				      return Observable.of(updateAuthInfo(userInfo))
+			      }
 			      return Observable.empty()
 		      })
       })
--- src/services/fireAuth/reducer.js
+++ src/services/fireAuth/reducer.js
@@ -8,13 +8,13 @@ const initialState = fromJS({
 	firstName: null,
 	lastName: null,
 	emailVerified: false,
-	uuid: null
+	uid: null
 })
 export default (state = initialState, {type = null, payload = null}) => {
 	switch (type) {
 		case actionTypes.UPDATE_USER_AUTH_INFO: {
 			console.log('payload: ', payload)
-			return state.set(['authed'], true)
+			return state.merge(payload)
 		}
 	}
 	return state
